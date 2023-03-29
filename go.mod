7b2a046ddabe9cc3726ebedf94b88dc2f0f19138
rayepeng <rayepeng@tencent.com>
2023-03-29 19:31:01 +0800

    feat: 🎸 add simplecmdtool

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.20
 
 require (
 	github.com/sirupsen/logrus v1.9.0
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.8.2
 	github.com/urfave/cli/v2 v2.25.1
 )
 
@@ -12,6 +12,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rayepeng/simplecmdtool v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- main.go
+++ main.go
@@ -4,49 +4,37 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/rayepeng/simplecmdtool/tools"
 	"github.com/rayepeng/wecombot/wecom"
 	"github.com/urfave/cli/v2"
 )
 
-func createApp() *cli.App {
-	app := &cli.App{
-		Name:  "wecom",
-		Usage: "Send messages to WeCom",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "access_token",
-				Usage:    "Access token for WeCom",
-				Required: true,
-			},
-			&cli.StringFlag{
-				Name:     "text",
-				Usage:    "Text message to send",
-				Required: true,
-			},
-		},
-		Action: func(c *cli.Context) error {
-			accessToken := c.String("access_token")
-			text := c.String("text")
+func PushWeComMsg(ctx *cli.Context) error {
+	accessToken := ctx.String("a")
+	text := ctx.String("m")
 
-			msg := wecom.NewTextMessage(text)
-			client := wecom.NewClient(accessToken)
-			err := client.Send(msg)
-			if err != nil {
-				return err
-			}
-
-			fmt.Println("Message sent successfully")
-			return nil
-		},
+	msg := wecom.NewTextMessage(text)
+	client := wecom.NewClient(accessToken)
+	err := client.Send(msg)
+	if err != nil {
+		return err
 	}
-	return app
+
+	fmt.Println("Message sent successfully")
+	return nil
 }
 
 func main() {
-	app := createApp()
-	err := app.Run(os.Args)
+	tool := tools.NewTool([]*tools.FunctionConfig{
+		{
+			Name:        "PushWeComMsg",
+			Function:    PushWeComMsg,
+			OptionFlags: "a:m:",
+			Description: "-a <access_token> -m <msg>",
+		},
+	})
+	err := tool.Run(os.Args)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
+		fmt.Println(err)
 	}
 }
